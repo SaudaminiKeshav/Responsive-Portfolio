556a6b4f86a0ef3fd81bfd4cf8a3faeabe13ec4c
Saudamini Keshav <saudamini.keshav@gmail.com>
2020-08-09 16:10:26 -0700

    Updated page styling for all the pages

--- portfolio-script.js
+++ portfolio-script.js
@@ -3,7 +3,6 @@ $(document).ready(function(){
 
 carouselImageClickListener();
 
-portfolioImgaeClickListener();
 });
 
 function carouselImageClickListener(){
@@ -12,15 +11,25 @@ console.log("code quiz clicked");
 });
 }
 
-// function portfolioImgaeClickListener(){
-   
-//         $(".vc").click(function(){
-//           $(".vc").animate({
-//             left: '250px',
-//             opacity: '1',
-//             height: '150%',
-//             width: '150%'
-//           });
-//         });
-      
-// }
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = $(".vc");
+var modalImg = document.getElementById("img01");
+
+img.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() { 
+  modal.style.display = "none";
+}
+
+var vid = $("video");
+vid.autoplay = true;
